16b4ac16f6420be195b9aec9952b18d6672d15da
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-07 23:24:53 +0330

    added language key

---
+++ lang/en/base.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Sms Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Sms for various
+    | messages that we need to display to the user.
+    |
+    */
+
+    "validation" => [
+    ],
+
+    "messages" => [
+    ],
+
+    "exceptions" => [
+    ],
+
+];
