e1f2dc094116bfc9394166a1f5bdc134c9d87d94
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-07 23:26:43 +0330

    added sms service provider

---
+++ src/SmsServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace JobMetric\Sms;
+
+use JobMetric\PackageCore\Exceptions\MigrationFolderNotFoundException;
+use JobMetric\PackageCore\Exceptions\RegisterClassTypeNotFoundException;
+use JobMetric\PackageCore\PackageCore;
+use JobMetric\PackageCore\PackageCoreServiceProvider;
+
+class SmsServiceProvider extends PackageCoreServiceProvider
+{
+    /**
+     * @param PackageCore $package
+     *
+     * @return void
+     * @throws MigrationFolderNotFoundException
+     * @throws RegisterClassTypeNotFoundException
+     */
+    public function configuration(PackageCore $package): void
+    {
+        $package->name('laravel-sms')
+            ->hasConfig()
+            ->hasMigration()
+            ->hasTranslation()
+            ->registerClass('Sms', Sms::class);
+    }
+}
