b930b3abcc4f694221fac96c06b7206e126ecd70
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-10 17:24:00 +0330

    added some key in language files

--- lang/en/base.php
+++ lang/en/base.php
@@ -27,6 +27,19 @@ return [
         "sms_gateway_not_found" => "The SMS gateway with number :number was not found.",
     ],
 
+    "list" => [
+        "sms_gateway" => [
+            "title" => "SMS Gateway",
+            "description" => "List of SMS gateways used on the website, always one gateway must be selected as default to send SMS through it.",
+            "filters" => [
+                "name" => [
+                    "title" => "Name",
+                    "placeholder" => "Search by name",
+                ],
+            ],
+        ],
+    ],
+
     "form" => [
         "fields" => [
             "name" => [
